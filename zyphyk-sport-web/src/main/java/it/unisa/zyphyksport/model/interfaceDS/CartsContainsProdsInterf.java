d28d9b7f1766152fb5db61a95441aed42b55c17e
cirovitale <cirovitale@pm.me>
2023-01-13 21:57:58 +0100

    Modify CartsContainsProds, implements servlets
    added servlets: GetProdsFromCartServlet, RemFromCartServlet,
    ModQuantityInCartServlet

--- zyphyk-sport-web/src/main/java/it/unisa/zyphyksport/model/interfaceDS/CartsContainsProdsInterf.java
+++ zyphyk-sport-web/src/main/java/it/unisa/zyphyksport/model/interfaceDS/CartsContainsProdsInterf.java
@@ -2,15 +2,16 @@ package it.unisa.zyphyksport.model.interfaceDS;
 
 import java.sql.SQLException;
 import java.util.Collection;
-
 import it.unisa.zyphyksport.model.bean.CartsContainsProdsBean;
 
 public interface CartsContainsProdsInterf {
 	public void doSave(int cartId, String productId, int quantity, int size) throws SQLException;
 	
-	public void doUpdate(int cartId, String productId, int quantity, int size) throws SQLException;
+	public void doUpdate(int cartId, String productId, int quantity) throws SQLException;
 	
 	public void doDelete(int cartId, String productId, int size) throws SQLException;
+	
+	public CartsContainsProdsBean doRetrieveByKey(int cartId, String productId, int size) throws SQLException;
 
 	public Collection<CartsContainsProdsBean> doRetrieveAll(String order) throws SQLException;
 	
